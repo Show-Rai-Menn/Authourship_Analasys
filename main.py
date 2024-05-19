f198eba26788b050502327c39b0f7ad7d7a4c103
Hirotaka-0926 <abehirotaka94@gmail.com>
2024-05-19 16:16:29 +0900

    レベル1のtask2のGUI化

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, f
 import os
 from werkzeug.utils import secure_filename
 import db
-import search_token
+import analysis
 app = Flask(__name__)
 
 @app.route('/')
@@ -69,7 +69,7 @@ def select_file():
     return render_template('select.html', namelist=filename)
 
 @app.route('/token', methods=['POST'])
-def token():
+def token_frequency():#ここでは選択されたファイルを受け取ってtoken frequencyの処理をしている
     try:
         print("success access /token")
         selected=request.form.getlist("option")
@@ -79,16 +79,46 @@ def token():
         for filename in selected:
             filepath=os.path.join("uploads", filename)
             print(counter*20)
-            tokenlist=search_token.search_token(filepath, counter*20)
+            tokenlist=analysis.token_frequency(filepath, counter*20)
             if tokenlist is None:
-                tokenlist=[]
+                tokenlist=[]#listの初期化
                 print("tokenlist is none")
-            filelist.append(filename)
+            filename="".join(filename)
+            filelist.append(filename)#listの中に[filename, tokenlist]で値を格納
             filelist.append(tokenlist)
             print("get filelist")
             print(filelist)
         return render_template('token.html', filelist=filelist, analysis_method="search frequency token", token_num=counter*20)
+    #ここではtoken.htmlはselect.htmlのid=resultに埋め込んでいる
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "An error occurred", 500
     
+
+@app.route('/display/search', methods=['POST'])
+def search_display():
+    return render_template('display.html', type=request.form.get("type"))
+    
+
+@app.route('/token/search', methods=['POST'])
+def token_search():
+    try:
+        keyword=request.form.get("keyword")
+        print("success access /token/search")
+        selected=request.form.getlist("option")
+        filelist=[]
+        for filename in selected:
+            filepath=os.path.join('uploads', filename)
+            sentence_list=analysis.token_search(keyword, filepath)
+
+            if sentence_list is None:
+                sentence_list=[]
+                print("sentence is none")
+            filename="".join(filename)
+            filelist.append(filename)
+            filelist.append(sentence_list)
+        return render_template('token.html', filelist=filelist, analysis_method="token_search", keyword=keyword)
+            
     except Exception as e:
         print(f"An error occurred: {e}")
         return "An error occurred", 500
