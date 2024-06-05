a25c79a5fa0d23a17d044a1e12f377e54f51d397
Hirotaka-0926 <abehirotaka94@gmail.com>
2024-06-06 01:26:49 +0900

    K_and_Kの修正とQ vs K の実装

---
+++ static/javascript/comparison.js
@@ -0,0 +1,39 @@
+let count=0;
+
+function comparison() {
+    const form = document.getElementById('form');
+    const data = {
+        Q: [],
+        K: [],
+        count:0
+    };
+    count++;
+    // 選択されているチェックボックスを取得
+    const checkboxes = form.querySelectorAll('input[type="checkbox"]:checked');
+    
+
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.name === 'Q') {
+            data.Q.push(checkbox.value);
+        } else if (checkbox.name === 'K') {
+            data.K.push(checkbox.value);
+        }
+    });
+    data.count=count;
+    fetch('/comparison/result', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        const resultTag = document.getElementById('result');
+        resultTag.innerHTML = result.html;
+    })
+    .catch(error => {
+        const resultTag = document.getElementById('result');
+        resultTag.innerHTML = 'An error occurred: ' + error.message;
+    });
+}
